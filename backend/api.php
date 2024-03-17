8285b1e125fb13be59744e9570d33bcbeb5b3788
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-17 13:38:01 +0200

    finished the database and php and basic html

---
+++ backend/api.php
@@ -0,0 +1,59 @@
+<?php
+
+header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header('Content-Type: application/json');
+
+    $mysqli = new mysqli('localhost', 'root', "", "todo");
+
+    if($mysqli->connection_error){
+        die("Connection Error (" . $mysqli->connect_errno . ')' . $mysqli->connect_error);
+    }
+
+
+    $request_method = $_SERVER["REQUEST_METHOD"];
+
+    switch ($request_method) {
+        case 'GET':
+                $response = getAllNews();
+                echo json_encode($response);
+            break;
+
+
+        case 'POST':
+            if(!empty($_POST["text"]) && !empty($_POST["type"])){
+                $text= $_POST["text"];
+                $type= $_POST["type"];
+                $response = createNew($text,$type);
+                echo json_encode($response);
+            }else{
+                echo json_encode([
+                    "status"=>"text is requried",
+                ]);
+            }
+
+            break;
+            
+        default:
+            echo json_encode([
+                "status"=>"something went wrong",
+            ]);
+            break;
+    }
+
+    function createNew($text,$type){
+        global $mysqli;
+        $response;
+        $query = $mysqli->prepare("INSERT INTO news (text,type) VALUES (?,?)");
+        $query->bind_param("ss", $text,$type);
+        if($query->execute()){
+            $response["status"] = "Success";
+        }else{
+            $response["status"] = "Failed";
+        }
+
+        return $response;
+    }
---
+++ frontend/scripts/index.js
@@ -0,0 +1,9 @@
+const news_form=document.getElementById('news-form');
+const news_input=document.getElementById('news-input'); 
+const news_list=document.getElementById('news-list');
+const news_div=document.getElementById('news-div');
+
+
+
+
+  
